a9cee4565913fc4170d75526695a5c7975e6469f
bogdan4402-cpu <bogdan4402@proton.me>
2026-03-08 21:12:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.environ["BOT_TOKEN"]
-DB_PATH   = os.environ.get("DB_PATH", "music.db")
+BOT_TOKEN  = os.environ["BOT_TOKEN"]
+DB_PATH    = os.environ.get("DB_PATH", "music.db")
+ADMIN_ID   = 1106966008
