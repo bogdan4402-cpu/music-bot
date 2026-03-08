a8cbc915135a5ad397789cd5182b14f6293f7eec
bogdan4402-cpu <bogdan4402@proton.me>
2026-03-08 20:06:16 +0200

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+DB_PATH   = os.environ.get("DB_PATH", "music.db")  # можно переопределить через Railway Variables
